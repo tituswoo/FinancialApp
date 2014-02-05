2ec0dd62e0fed889478c526dfca4d2aef64c13cd
Vishal <vishalxv@gmail.com>
2014-02-05 09:34:53 -0500

    completed M3 login functionality

--- src/com/classwarfare/financialapp/MainActivity.java
+++ src/com/classwarfare/financialapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.classwarfare.financialapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +22,8 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void openLoginActivity(View view) {
+    	Intent intent = new Intent(this, LoginActivity.class);
+    	startActivity(intent);
+    }
 }
