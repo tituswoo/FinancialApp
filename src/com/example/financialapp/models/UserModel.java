b18c1eddb1788cd58ecbaf103f11aa150a7c218b
Vishal <vishalxv@gmail.com>
2014-02-14 15:34:49 -0500

    Changed UserModel to be static.

--- src/com/example/financialapp/models/UserModel.java
+++ src/com/example/financialapp/models/UserModel.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 public class UserModel {
 	
-	private ArrayList<User> users = new ArrayList<User>();
+	private static ArrayList<User> users;
 	
-	public UserModel() {
+	static {
+		users = new ArrayList<User>();
 		users.add(new Admin("admin")); // add a default admin user.
 	}
 	
-	public boolean userExists(String username, String password) {
+	public static boolean userExists(String username, String password) {
 		for (User user : users) {
 			if (user.getUsername().equals(username) &&
 					user.getPassword().equals(password)) {
@@ -18,5 +19,18 @@ public class UserModel {
 			}
 		}
 		return false;
+	}
+	/**
+	 * 
+	 * @param newUser - the new user to be added to the user list
+	 * @return true if the user was successfully added, false if the user cannot be added
+	 */
+	public static boolean addUser(User newUser) {
+		for(User user: users) {
+			if(user.getUsername().equals(newUser.getUsername())) {
+				return false;
+			}
+		}
+		return users.add(newUser);
 	}
 }
