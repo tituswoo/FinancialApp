3a39808122c12ecf7719769d1fe401a06e98e9d1
tadros3 <tadros3@gatech.edu>
2014-04-02 18:43:07 -0400

    Created test cases for the Transaction class.

---
+++ src/com/example/financialapp/models/TransactionTest.java
@@ -0,0 +1,41 @@
+package com.example.financialapp.models;
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for the Transaction class.
+ * @author Toma Tadros
+ *
+ */
+
+public class TransactionTest extends TestCase {
+
+	public void setUp() {
+		Transaction t = new Transaction(100, "Description", "Category");
+	}
+	
+	public void testCategory() {
+		Transaction t = new Transaction(100, "Desc", "Food");
+		assertEquals(t.getCategory(), "Food");
+		
+		t.setCategory("Rent");
+		assertEquals(t.getCategory(), "Rent");
+	}
+	
+	public void testDescription() {
+		Transaction t = new Transaction(100, "Taco Bell", "Food");
+		assertEquals(t.getDescription(), "Taco Bell");
+		
+		t.setDescription("Subway");
+		assertEquals(t.getDescription(), "Subway");
+	}
+	
+	public void testAmount() {
+		Transaction t = new Transaction(100, "Desc", "Cat");
+		assertEquals(t.getAmount(), 100);
+		
+		t.setAmount(99.99);
+		assertEquals(t.getAmount(), 99.99);
+	}
+
+}
