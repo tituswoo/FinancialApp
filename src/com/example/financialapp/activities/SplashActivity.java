b0fce9ad0f3ad713167639ffd1f1d709d934eb16
TItus Woo <titus.woo@me.com>
2014-04-24 09:51:00 -0400

    more nonfunctional stuff

--- src/com/example/financialapp/activities/SplashActivity.java
+++ src/com/example/financialapp/activities/SplashActivity.java
@@ -1,9 +1,10 @@
 package com.example.financialapp.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
-import android.widget.ProgressBar;
 
 import com.example.financialapp.R;
 
@@ -13,7 +14,14 @@ public class SplashActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-		
+		/*new Handler().postDelayed(new Runnable(){
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		}, 500);*/
 		this.finish();
 	}
 
